92524d92bd8992be46477725d7e0548c30ff8b31
Taja House <ahlliahtaja@yahoo.com>
2020-07-26 16:21:07 -0700

    Setting up migration files

---
+++ migrations/20200726155438_create-cars-table.js
@@ -0,0 +1,19 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('cars', tbl=>{
+      tbl.increments('id'); //creates an auto-incrementing primary key. Most of our constraints would start with this line of code.
+      tbl.text('make', 120)
+        .notNullable();
+      tbl.text('model', 120)
+        .notNullable();
+      tbl.text('VIN', 17)
+        .notNullable();
+      tbl.int('mileage', 6)
+        .notNullable()
+      tbl.text('')
+  })
+}; // The change we make to our schema
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('cars')
+}; // Undoing changes
